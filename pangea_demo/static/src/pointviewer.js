5548c6df239739b2ffd91c25a82f19252d33ffd5
ScottPeachey <79957133+ScottPeachey@users.noreply.github.com>
2022-05-16 10:03:33 +1000

    added comments

--- pangea_demo/static/src/pointviewer.js
+++ pangea_demo/static/src/pointviewer.js
@@ -50,11 +50,13 @@ async function loadData() {
     setText('focal_length', viewFocalLength);
 
     // TODO: load /data/points.json into pointData
+    // Getting the data
     let response = await fetch('/data/points.json', {
         headers: {
             'Accept': 'application/json'
         }
     });
+    // Loading data into pointData variable
     pointData = await response.json();
 
     // TODO: connect dataSelect to different point datasets. Either add data to points.json or load multiple points
